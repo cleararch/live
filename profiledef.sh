41375148bcdb03ad40d89dad9266be5e792f6fc9
yjacks <jackeyz2022@mail.ru>
2023-02-25 13:37:34 +0800

    第一个版本

--- profiledef.sh
+++ profiledef.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name="CALIVE"
+iso_label="ClearArch_$(date +%Y%m)"
+iso_publisher="ClearArch <https://cleararch.github.io>"
+iso_application="ClearArch Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
-           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
-           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+bootmodes=('bios.syslinux.mbr'
+           'uefi-x64.grub.esp'
+           'uefi-x64.grub.eltorito'
+           )
 arch="x86_64"
 pacman_conf="pacman.conf"
-airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_type="erofs"
+airootfs_image_tool_options=('-comp' 'zstd' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
