5b8edbae07b39e885fb235fa11391c15ae611b65
yjacks <jackeyz2022@mail.ru>
2023-02-25 15:53:22 +0800

    完成第一次构建

--- profiledef.sh
+++ profiledef.sh
@@ -9,13 +9,14 @@ iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr'
+           'bios.syslinux.eltorito'
            'uefi-x64.grub.esp'
            'uefi-x64.grub.eltorito'
            )
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="erofs"
-airootfs_image_tool_options=('-comp' 'zstd' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-zlzma,9' -E ztailpacking)
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
