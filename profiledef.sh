c1615dc787ae2f1baf9be1ed47521751003a7a92
yjacks <jackeyz2022@mail.ru>
2023-03-13 21:35:49 +0800

    第一次提交（伪）

---
+++ profiledef.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="CALIVE"
+iso_label="ClearArch_$(date +%Y%m)"
+iso_publisher="ClearArch <https://cleararch.github.io>"
+iso_application="ClearArch Live/Rescue CD"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr'
+           'bios.syslinux.eltorito'
+           'uefi-x64.systemd-boot.eltorito'
+           'uefi-x64.systemd-boot.esp'
+           )
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="erofs"
+#airootfs_image_tool_options=('-zlzma,9' -E ztailpacking)
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
+  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/livecd-sound"]="0:0:755"
+)
